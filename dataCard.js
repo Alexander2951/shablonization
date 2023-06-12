a77335dc9b90a1ae56613762a61a0faa8896bb6b
Alexander <alex-lama@yandex.ru>
2023-06-12 17:35:37 +0300

    ver12

---
+++ dataCard.js
@@ -0,0 +1,15 @@
+export const dataProducts = `[{
+    "name":" MANGO PEPLE T-SHIRT",
+    "image": "img/product1.jpg",
+    "price": 300,
+    "color": "Red",
+    "size": "Xl",
+    "quantity": 2
+},{
+    "name":" ellery x m'o capsule",
+    "image": "img/product2.jpg",
+    "price": 52,
+    "color": "Black",
+    "size": "Xl",
+    "quantity": 2
+}]`;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { products } from "./data.js";
 const product = JSON.parse(products);
 const productBox = document.querySelector('.productions');
+const productBox2 = document.querySelector('.product_box');
 product.forEach(({name, image, price, disclaimer}) => {
     const productEl = `<div class="product">
     <div><img src="${image}"/></div>
@@ -9,4 +10,68 @@ product.forEach(({name, image, price, disclaimer}) => {
     <p class="price"> $ ${price}</p>
   </div>`
   productBox.insertAdjacentHTML("beforeend", productEl);
+  });
+/*
+productBox.addEventListener('mouseover', () => {
+  const addToCart = document.createElement('button');
+  addToCart.textContent = 'Add To Cart';
+  productBox.appendChild(addToCart);
+})
+*/
+const addCart = document.querySelectorAll('.product');
+addCart.forEach(buttonEl =>  {
+  buttonEl.addEventListener('mouseover', () => {
+  const button =  document.createElement('button');
+    
+    button.textContent = 'Add To Cart'
+    buttonEl.appendChild( button);
+    buttonEl.addEventListener('mouseout', () => {
+  
+      buttonEl.removeChild(button);
+      }
+  )
+  
+  });
+  buttonEl.addEventListener('click', () => {
+    const newCart = document.createElement('div');
+    
+    newCart.classList.add('product');
+   productBox2.appendChild( newCart)
+   newCart.appendChild(productData)
+    })
 });
+
+import { dataProducts } from "./dataCard.js";
+const productData = JSON.parse(dataProducts);
+
+
+
+productData.forEach(({name, image, price, color, size, quantity}) => {
+  const productEl2 =` 
+  <div class="product">
+      <button class="btn_del" type="button">Удалить</button>
+      <div class="product_content">
+        <img class="product_img" src="${image}" alt="${name}" srcset="" />
+        <div class="product_desc">
+          <h2 class="product_name">${name}</h2>
+          <p class="product_price_lable">
+            Price:<span class="product_price"> ${price}</span>
+          </p>
+          <p class="product_color">Color: ${color}</p>
+          <p class="product_size">Size: ${size}</p>
+          <div class="product_qty">
+            <label class="input_label">Quantity:</label>
+            <input class="input_quantity" type="number" value="${quantity}">
+          </div>
+        </div>
+      </div>
+    </div>`;
+    productBox2.insertAdjacentHTML('beforeend', productEl2)
+});
+const deleteButtons = document.querySelectorAll('.btn_del');
+deleteButtons.forEach(button => {
+  button.addEventListener('click', () => {
+      const product = button.closest('.product');
+      product.remove();
+  })
+})
